8ffa7c6a40dbdb7fcafe37b4f9c0d5fd2bcaaf89
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-10-25 15:42:48 +0700

    add many things

--- layouts/common.js
+++ layouts/common.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const name = "Pirate";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Fundamental analysis club" />
@@ -22,44 +22,18 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+      <Navbar transparent />
 
       <main>{children}</main>
-      {!home && (
+      {/* {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
             <a>← Back to home</a>
           </Link>
         </div>
-      )}
-    </div>
+      )} */}
+
+      <Footer />
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/global.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
