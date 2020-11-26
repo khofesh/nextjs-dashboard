7657c0310fafbbd31b2e70bb0aa08813992dde5f
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-11-26 21:39:54 +0700

    dynamic routes

---
+++ pages/posts/[id].js
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../layouts/common";
+import { getPostData, getAllPostIds } from "../../lib/posts";
+
+export default function FirstPost(props) {
+  const { postData } = props;
+
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <div
+        className="relative pt-16 pb-32 flex content-center items-center justify-center flex-col"
+        style={{
+          minHeight: "75vh",
+        }}
+      >
+        <div>
+          <h2 style={{ color: "blue" }}>
+            <Link href="/">
+              <a>Back home</a>
+            </Link>
+          </h2>
+        </div>
+
+        <div style={{ flex: 1 }}>
+          <div style={{ margin: 20, textAlign: "center" }}>
+            <div>
+              {postData.title}
+              <br />
+              {postData.id}
+              <br />
+              {postData.date}
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
